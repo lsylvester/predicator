a23403859b49d0fa2d57cc45c6f01743b5280809
lsylvester <lachlan.sylvester@hypothetical.com.au>
2013-12-06 15:45:24 +1100

    remove modules from the ColumnQueryChain class

--- lib/predicator.rb
+++ lib/predicator.rb
@@ -32,31 +32,31 @@ module Predicator
     def method_missing *args, &block
       @scope.where!(column).send(*args, &block)
     end
-
-    module QueryMethods
-
-      def where(opts = :chain, *rest)
-        if opts == :chain
-          WhereChain.new(spawn)
-        elsif !opts.is_a?(column_where_chain_class) && rest.empty?
-          column_where_chain_class.new(spawn, opts)
-        elsif opts.blank?
-          self
-        else
-          spawn.where!(opts, *rest)
-        end
-      end
-    end
-
-    module Predicate
-      def column_where_chain_class
-        return ColumnWhereChain if self == ActiveRecord::Base
-        @column_where_chain_class ||=  Class.new(superclass.column_where_chain_class)
-      end
-
-      delegate :predicate, to: :column_where_chain_class
-    end
-    ::ActiveRecord::Relation.send :include, QueryMethods
-    ::ActiveRecord::Base.send :extend, Predicate
   end
+
+  module QueryMethods
+
+    def where(opts = :chain, *rest)
+      if opts == :chain
+        WhereChain.new(spawn)
+      elsif !opts.is_a?(column_where_chain_class) && rest.empty?
+        column_where_chain_class.new(spawn, opts)
+      elsif opts.blank?
+        self
+      else
+        spawn.where!(opts, *rest)
+      end
+    end
+  end
+
+  module Predicate
+    def column_where_chain_class
+      return ColumnWhereChain if self == ActiveRecord::Base
+      @column_where_chain_class ||=  Class.new(superclass.column_where_chain_class)
+    end
+
+    delegate :predicate, to: :column_where_chain_class
+  end
+  ::ActiveRecord::Relation.send :include, QueryMethods
+  ::ActiveRecord::Base.send :extend, Predicate
 end
